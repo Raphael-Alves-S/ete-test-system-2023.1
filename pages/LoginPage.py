73574715d4579968e86a7f053178fb0da3bd6d60
TalesVGS <tales.santana@aluno.unievangelica.edu.br>
2023-11-11 10:11:19 -0300

    criando teste de login

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,34 +1,39 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 from pages.PageObject import PageObject
 
 
 class LoginPage(PageObject):
 
-    url = 'https://www.saucedemo.com/'
+    url = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
 
     def __init__(self, browser):
         super(LoginPage, self).__init__(browser=browser)
+        self.WebDriverWait = None
 
     def open_page(self):
         self.driver.get(self.url)
 
     def click_login_btn(self):
-        self.driver.find_element(By.ID, 'login-button').click()
+        self.driver.find_element(By.CSS_SELECTOR, '.oxd-button').click()
 
     def is_url_login(self):
         return self.is_url(self.url)
 
-    def has_login_error_message(self):
-        error_message = self.driver.find_element(By.CLASS_NAME, 'error-message-container').text
-        return error_message == 'Epic sadface: Username is required'
+    # def has_login_error_message(self):
+    #     error_message = self.driver.find_element(By.CLASS_NAME, 'error-message-container').text
+    #     return error_message == 'Epic sadface: Username is required'
 
-    def is_page(self, url, title_text):
-        raise Exception('Essa página nao tem titutlo')
+    # def is_page(self, url, title_text):
+    #     raise Exception('Essa página nao tem titutlo')
 
-    def enter_login(self, user_name='standard_user', password='secret_sauce'):
-        self.driver.find_element(By.ID, 'user-name').send_keys(user_name)
-        self.driver.find_element(By.CSS_SELECTOR, '[data-test="password"]').send_keys(password)
+    def enter_login(self, user_name='Admin', password='admin123'):
+        self.wait_visible_element(By.NAME, 'username', 10)
+        self.driver.find_element(By.NAME, 'username').send_keys(user_name)
+        self.driver.find_element(By.NAME, 'password').send_keys(password)
         self.click_login_btn()
 
--- tests/test_1.py
+++ tests/test_1.py
@@ -3,10 +3,9 @@ import pytest
 
 class Test1:
 
-    @pytest.mark.parametrize('all_browsers', ['chrome', 'safari', 'firefox'])
+    @pytest.mark.parametrize('all_browsers', ['chrome'])
     def test_click_login_button(self, run_all_browser):
         login_p = run_all_browser
-        login_p.click_login_btn()
-        assert login_p.is_url_login(), 'Aplicação não permaneceu na mesma página!'
-        assert login_p.has_login_error_message(), 'Mensagem de erro inválida!'
+        login_p.enter_login()
+
 
