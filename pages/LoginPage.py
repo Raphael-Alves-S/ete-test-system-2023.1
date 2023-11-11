257a943c11e775147d70740fceb714c00ff970e1
Raphael Alves <rapha.alves75@gmail.com>
2023-11-11 10:34:09 -0300

    Feature: Incluindo projeto base para consulta de padronização

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -8,8 +8,10 @@ from pages.PageObject import PageObject
 
 
 class LoginPage(PageObject):
-
     url = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+    css_selec_login_button = '.oxd-button'
+    selector_by_name_username = 'username'
+    selector_by_name_password = 'password'
 
     def __init__(self, browser):
         super(LoginPage, self).__init__(browser=browser)
@@ -18,22 +20,14 @@ class LoginPage(PageObject):
     def open_page(self):
         self.driver.get(self.url)
 
-    def click_login_btn(self):
-        self.driver.find_element(By.CSS_SELECTOR, '.oxd-button').click()
-
     def is_url_login(self):
         return self.is_url(self.url)
 
-    # def has_login_error_message(self):
-    #     error_message = self.driver.find_element(By.CLASS_NAME, 'error-message-container').text
-    #     return error_message == 'Epic sadface: Username is required'
-
-    # def is_page(self, url, title_text):
-    #     raise Exception('Essa página nao tem titutlo')
+    def click_login_btn(self):
+        self.driver.find_element(By.CSS_SELECTOR, self.css_selec_login_button).click()
 
     def enter_login(self, user_name='Admin', password='admin123'):
-        self.wait_visible_element(By.NAME, 'username', 10)
-        self.driver.find_element(By.NAME, 'username').send_keys(user_name)
-        self.driver.find_element(By.NAME, 'password').send_keys(password)
+        self.wait_visible_element(By.NAME, self.selector_by_name_username, 10)
+        self.driver.find_element(By.NAME, self.selector_by_name_username).send_keys(user_name)
+        self.driver.find_element(By.NAME, self.selector_by_name_password).send_keys(password)
         self.click_login_btn()
-
