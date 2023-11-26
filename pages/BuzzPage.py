4bee03fa417044b88a7908dbd74dce9ee20a6d3c
TalesVGS <tales.santana@aluno.unievangelica.edu.br>
2023-11-26 15:12:37 -0300

    alterações em buzzpage e criação do test2 do buzzpage

--- pages/BuzzPage.py
+++ pages/BuzzPage.py
@@ -6,8 +6,15 @@ class BuzzyPage(PageObject):
     url = 'https://opensource-demo.orangehrmlive.com/web/index.php/buzz/viewBuzz'
     class_buzz_tittle = 'oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module'
     css_buzzy_input = '.oxd-buzz-post-input'
-    xpath_buzz_post = "afa//b[.='Tjeste de post']"
     xpath_btn_post = "//button[@class='oxd-button oxd-button--medium oxd-button--main']"
+    xpath_btn_three_dots = "//div[@class='oxd-grid-1 orangehrm-buzz-newsfeed-posts']/div[1]//div[@class='orangehrm-buzz-post']//button[@class='oxd-icon-button']"
+    css_btn_edit_post = '.bi-pencil'
+    xpath_buzz_post = "//p[.='Teste de post']"
+
+    xpath_modal_tittle = "//p[.='Edit Post']"
+    css_input_edit = '.oxd-buzz-post--composing > .oxd-buzz-post-input'
+    xpath_text_area = "//textarea[.='Teste de postEditado']"
+    xpath_post_edit = "//div[@class='oxd-form-actions orangehrm-buzz-post-modal-actions']/button[@class='oxd-button oxd-button--medium oxd-button--main']"
 
     def __init__(self, driver):
         super(BuzzyPage, self).__init__(driver=driver)
@@ -27,10 +34,28 @@ class BuzzyPage(PageObject):
     # ----------------------------- Fazer um post ---------------------------- #
 
     def buzzy_post(self, post='Teste de post'):
-        self.wait_visible_element(By.CSS_SELECTOR,  self.css_buzzy_input, 5)
+        self.wait_visible_element(By.CSS_SELECTOR,  self.css_buzzy_input, 7)
         self.driver.find_element(By.CSS_SELECTOR,  self.css_buzzy_input).send_keys(post)
         self.driver.find_element(By.XPATH, self.xpath_btn_post).click()
 
     def view_post(self):
-        return self.wait_visible_element(By.XPATH, self.xpath_buzz_post, 5)
+        self.wait_visible_element(By.XPATH, self.xpath_buzz_post, 7)
+        self.driver.find_element(By.XPATH, self.xpath_buzz_post).is_displayed()
 
+    # ----------------------------- Editar um post ---------------------------- #
+
+    def edit_post(self, edit="Editado"):
+        self.wait_visible_element(By.XPATH, self.xpath_btn_three_dots, 7)
+        self.driver.find_element(By.XPATH, self.xpath_btn_three_dots).click()
+
+        self.driver.find_element(By.CSS_SELECTOR, self.css_btn_edit_post).is_displayed()
+        self.driver.find_element(By.CSS_SELECTOR, self.css_btn_edit_post).click()
+
+        self.wait_visible_element(By.XPATH, self.xpath_modal_tittle, 7)
+
+        self.wait_visible_element(By.CSS_SELECTOR, self.css_input_edit, 7)
+        self.driver.find_element(By.CSS_SELECTOR, self.css_input_edit).send_keys(edit)
+        self.wait_visible_element(By.XPATH, self.xpath_text_area, 7)
+
+        self.wait_visible_element(By.XPATH, self.xpath_post_edit, 7)
+        self.driver.find_element(By.XPATH, self.xpath_post_edit).click()
